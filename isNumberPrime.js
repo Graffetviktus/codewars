edca2a94eb0320d374e41aafca1ff2ce734ef34a
Paul Koul <graffetviktus@mail.ru>
2022-08-22 03:37:20 +0700

    solve number prime kata

--- isNumberPrime.js
+++ isNumberPrime.js
@@ -1,3 +1,13 @@
-function isPrime(num) {
-  //TODO
-}
+    function isPrime(num) {
+        let chekNum = Math.sqrt(num)
+
+        if(num < 2) {
+            return false;
+        }
+        for(let i = 2; i <= chekNum; i++) {
+            if(num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
